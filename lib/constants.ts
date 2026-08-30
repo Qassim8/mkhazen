dec68ac1bedae583a4f1570c3521d35e9bbb252d
qassim8 <mohammedalgasim123@gmail.com>
2026-08-30 14:35:55 +0200

    changed APP_URL at production

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
 export const MAIN_BRANCH_ID = "dcc40a00-1275-463f-9cd8-caf5487100b0";
 
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
+export const BASE_URL = process.env.APP_URL || "http://localhost:3000";
